21838f2c0d3259b24ea4359cc048d9a167ea39ab
ustymenko-d <ustymenko.denys@gmail.com>
2025-01-15 22:35:13 +0200

    Remove unused 'gulp-newer' import and update its usage in the images task

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,9 +1,7 @@
 import replace from 'gulp-replace';
 import browsersync from 'browser-sync';
-import newer from 'gulp-newer';
 
 export const plugins = {
 	replace: replace,
 	browsersync: browsersync,
-	newer: newer,
 };
--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,14 +1,15 @@
 import webp from 'gulp-webp';
 import imagemin from 'gulp-imagemin';
+import newer from 'gulp-newer';
 
 export const images = () => {
 	return app.gulp
 		.src(app.path.src.images)
-		.pipe(app.plugins.newer(app.path.build.images))
+		.pipe(newer(app.path.build.images))
 		.pipe(webp())
 		.pipe(app.gulp.dest(app.path.build.images))
 		.pipe(app.gulp.src(app.path.src.images))
-		.pipe(app.plugins.newer(app.path.build.images))
+		.pipe(newer(app.path.build.images))
 		.pipe(
 			imagemin({
 				progressive: true,
