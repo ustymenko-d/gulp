b59f57b380ea4d80a7c27f88388b00c66e638bb0
ustymenko-d <ustymenko.denys@gmail.com>
2025-05-31 15:57:24 +0300

    Remove old code

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -72,64 +72,6 @@ export const copyWoffAndWoff2 = () =>
 		.src(`${app.path.src.fonts}*.{woff,woff2}`)
 		.pipe(app.gulp.dest(`${app.path.build.fonts}`))
 
-// export const generateFontStyles = () => {
-// 	let fontsStylesFile = `${app.path.srcFolder}/css/fonts.scss`
-
-// 	const getFontWeight = (fontFileName) => {
-// 		const sortedKeys = Object.keys(fontWeightMapping).sort(
-// 			(a, b) => b.length - a.length
-// 		)
-
-// 		const weightKey = sortedKeys.find((key) =>
-// 			fontFileName.toLowerCase().includes(key)
-// 		)
-
-// 		return weightKey ? fontWeightMapping[weightKey] : 400
-// 	}
-
-// 	const writeFontFace = (fontName, fontFileName, fontWeight) => {
-// 		const fontFace = `@font-face {
-// 				font-family: '${fontName}';
-// 				font-display: swap;
-// 				src: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");
-// 				font-weight: ${fontWeight};
-// 				font-style: normal;
-// 		}\r\n`
-// 		fs.appendFile(fontsStylesFile, fontFace, () => {})
-// 	}
-
-// 	fs.readdir(app.path.build.fonts, (error, fontsFiles) => {
-// 		if (error) {
-// 			console.error('Error reading fonts directory:', error)
-// 			return
-// 		}
-
-// 		if (fontsFiles && fontsFiles.length > 0) {
-// 			if (!fs.existsSync(fontsStylesFile)) {
-// 				fs.writeFileSync(fontsStylesFile, '')
-
-// 				let processedFonts = new Set()
-
-// 				fontsFiles.forEach((file) => {
-// 					const fontFileName = file.split('.')[0]
-
-// 					if (!processedFonts.has(fontFileName)) {
-// 						const fontWeight = getFontWeight(fontFileName)
-// 						writeFontFace(fontFileName, fontFileName, fontWeight)
-// 						processedFonts.add(fontFileName)
-// 					}
-// 				})
-// 			} else {
-// 				console.log(
-// 					'The css/fonts.scss file already exists. Delete it to regenerate.'
-// 				)
-// 			}
-// 		}
-// 	})
-
-// 	return app.gulp.src(`${app.path.srcFolder}`)
-// }
-
 export const generateFontStyles = (done) => {
 	const fontsStylesFile = path.join(app.path.srcFolder, 'css', 'fonts.scss')
 
